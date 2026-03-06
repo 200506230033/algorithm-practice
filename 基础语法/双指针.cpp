f7ac7c3a4f3325ddacfb92f8246acff83f48d59a
Sakura <3263233120@qq.com>
2026-03-06 22:57:59 +0800

    3.6

---
+++ 基础语法/双指针.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+----------------------------------------
+蓝桥1371
+回文判断
+----------------------------------------
+*/
+bool ifhuiwen(const string& s){
+  int left = 0;
+  int right = s.size() - 1;
+
+  while(left < right){
+    if(s[left] != s[right])
+        return false;
+
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+void lanqiao1371(){
+  string s;
+  cin >> s;
+  cout << (ifhuiwen(s) ? 'Y' : 'N') << endl;
+}
+
+
+/*
+蓝桥1372
+美丽的区间
+*/
+void lanqiao1372(){
+    const int N = 1e5 + 10;
+    int a[N];
+
+    int n, S;
+    cin >> n >> S;
+
+    for(int i = 1; i <= n; i++){
+        cin >> a[i];
+    }
+
+    int l = 1, r = 1;
+    int sum = 0;
+    int ans = 1e9;
+
+    while(r <= n){
+        sum += a[r];
+
+        while(sum >= S){
+            ans = min(ans, r - l + 1);
+            sum -= a[l];
+            l++;
+        }
+
+        r++;
+    }
+
+    if(ans == 1e9)
+        cout << 0 << endl;
+    else
+        cout << ans << endl;
+
+}
+
+
+
+
+int main(){
+  lanqiao1371();
+  lanqiao1372();
+}
--- 基础语法/贪心.cpp
+++ 基础语法/贪心.cpp
@@ -1,21 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 1e5 + 9;
-int a[N];
 
 
 
 int main(){
-  int n;cin >> n;
-  for(int i = 1 ; i <= n ; i++){
-    cin >> a[i];
-  }
-  sort(a + 1 , a + n + 1);
-  int ans = 0;
-  for(int i = 1 ; i <= n ; i++){
-    ans = a[i] + a[i + 1];
-    a[i + 1] = ans;
-  }
-  cout << ans;
-  return 0;
+
+    return 0;
 }
+
+//蓝桥3412,545,532,2928
